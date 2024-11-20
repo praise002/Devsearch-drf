f52839d96f9d74875a37e0afbe88d49ba47f91ea
Praise Idowu <ifeoluwapraise02@gmail.com>
2024-11-21 00:06:57 +0100

    Dockerization

--- devsearch/settings/base.py
+++ devsearch/settings/base.py
@@ -30,12 +30,13 @@ SITE_ID = 1
 # Application definition
 
 DJANGO_APPS = [
-    # 'jazzmin',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",  # to enable whitenoise handle static files in development
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.postgres',
@@ -70,6 +71,7 @@ INTERNAL_IPS = [
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -153,7 +155,8 @@ STORAGES = {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage'
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        # "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
@@ -221,117 +224,117 @@ CORS_ALLOWED_ORIGINS = [
 
 # FRONTEND_URL = config("FRONTEND_URL")
 
-# JAZZMIN_SETTINGS = {
-#     # title of the window (Will default to current_admin_site.site_title if absent or None)
-#     "site_title": "DevSearch Admin",
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "DevSearch Admin",
 
-#     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-#     "site_header": "DevSearch",
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "DevSearch",
 
-#     # # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-#     "site_brand": "DevSearch",
+    # # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "DevSearch",
 
-#     # Logo to use for your site, must be present in static files, used for brand on top left
-#     "site_logo": "images/logo.svg",
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    "site_logo": "images/logo.svg",
 
-#     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-#     "login_logo": "images/logo.svg", 
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": "images/logo.svg", 
 
-#     # CSS classes that are applied to the logo above
-#     "site_logo_classes": "img-circle",  
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",  
 
-#     # Welcome text on the login screen
-#     "welcome_sign": "Welcome to DevSearch",
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome to DevSearch",
 
-#     # Copyright on the footer
-#     "copyright": "DevSearch Ltd",
+    # Copyright on the footer
+    "copyright": "DevSearch Ltd",
 
-#     # List of model admins to search from the search bar, search bar omitted if excluded
-#     # If you want to use a single search field you dont need to use a list, you can use a simple string 
-#     "search_model": "accounts.User", 
+    # List of model admins to search from the search bar, search bar omitted if excluded
+    # If you want to use a single search field you dont need to use a list, you can use a simple string 
+    "search_model": "accounts.User", 
     
-#     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
-#     "user_avatar": "photo",
+    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+    "user_avatar": "photo",
 
-#     ############
-#     # Top Menu #
-#     ############
+    ############
+    # Top Menu #
+    ############
 
-#     # Links to put along the top menu
-#     "topmenu_links": [
+    # Links to put along the top menu
+    "topmenu_links": [
 
-#         # Url that gets reversed (Permissions can be added)
-#         {
-#             "name": "Home",  
-#             "url": "admin:index", 
-#             "permissions": ["auth.view_user"]},
+        # Url that gets reversed (Permissions can be added)
+        {
+            "name": "Home",  
+            "url": "admin:index", 
+            "permissions": ["auth.view_user"]},
 
-#         # App with dropdown menu to all its models pages (Permissions checked against models)
-#         {"app": "accounts"},   
-#         {"app": "profiles"},  
-#         {"app": "projects"},
-#         {"app": "messaging"},
-#     ],
+        # App with dropdown menu to all its models pages (Permissions checked against models)
+        {"app": "accounts"},   
+        {"app": "profiles"},  
+        {"app": "projects"},
+        {"app": "messaging"},
+    ],
 
-#     #############
-#     # User Menu #
-#     #############
+    #############
+    # User Menu #
+    #############
 
-#     # Additional links to include in the user menu on the top right ("app" url type is not allowed)
-#     "usermenu_links": [
-#         {"name": "DevSearch Frontpage", "url": "/", "new_window": True},  
-#         {"model": "accounts.user"}  
-#     ],
+    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
+    "usermenu_links": [
+        {"name": "DevSearch Frontpage", "url": "/", "new_window": True},  
+        {"model": "accounts.user"}  
+    ],
 
-#     #############
-#     # Side Menu #
-#     #############
+    #############
+    # Side Menu #
+    #############
 
-#     # Whether to display the side menu
-#     "show_sidebar": True,
+    # Whether to display the side menu
+    "show_sidebar": True,
 
-#     # Whether to aut expand the menu
-#     "navigation_expanded": True,
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
 
-#     # Hide these apps when generating side menu e.g (auth)
-#     "hide_apps": [],
+    # Hide these apps when generating side menu e.g (auth)
+    "hide_apps": [],
 
-#     # Hide these models when generating side menu (e.g auth.user)
-#     "hide_models": [],
+    # Hide these models when generating side menu (e.g auth.user)
+    "hide_models": [],
 
-#     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-#     # for the full list of 5.13.0 free icon classes
-#     "icons": {
-#         "accounts.user": "fas fa-users-cog",
-#         "auth.Group": "fas fa-users",
-#         "sites.site": "fas fa-globe",
-#         "profiles.profile": "fas fa-id-card",  
-#         "profiles.skill": "fas fa-lightbulb",  
-#         "projects.project": "fas fa-tasks",  
-#         "projects.tag": "fas fa-tags",  
-#         "projects.review": "fas fa-star",  
-#         "messaging.message": "fas fa-envelope",
-#     },  
-#     # Icons that are used when one is not manually specified
-#     "default_icon_parents": "fas fa-chevron-circle-right",
-#     "default_icon_children": "fas fa-circle",
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "accounts.user": "fas fa-users-cog",
+        "auth.Group": "fas fa-users",
+        "sites.site": "fas fa-globe",
+        "profiles.profile": "fas fa-id-card",  
+        "profiles.skill": "fas fa-lightbulb",  
+        "projects.project": "fas fa-tasks",  
+        "projects.tag": "fas fa-tags",  
+        "projects.review": "fas fa-star",  
+        "messaging.message": "fas fa-envelope",
+    },  
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
 
-#     #################
-#     # Related Modal #
-#     #################
-#     # Use modals instead of popups
-#     "related_modal_active": False,
+    #################
+    # Related Modal #
+    #################
+    # Use modals instead of popups
+    "related_modal_active": False,
 
-#     #############
-#     # UI Tweaks #
-#     #############
-#     # Whether to show the UI customizer on the sidebar
-#     "show_ui_builder": False,
+    #############
+    # UI Tweaks #
+    #############
+    # Whether to show the UI customizer on the sidebar
+    "show_ui_builder": False,
 
-#     "changeform_format": "horizontal_tabs",
-#     # override change forms on a per modeladmin basis
-#     "changeform_format_overrides": {
-#         "auth.user": "collapsible", 
-#         "auth.group": "vertical_tabs"},
-# }
+    "changeform_format": "horizontal_tabs",
+    # override change forms on a per modeladmin basis
+    "changeform_format_overrides": {
+        "auth.user": "collapsible", 
+        "auth.group": "vertical_tabs"},
+}
 
